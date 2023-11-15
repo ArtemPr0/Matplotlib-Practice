78435191f1be1bd791584129203e2767af5224c9
Artem Prokhorov <artem.prokhorov@brushpowergen.ac.uk>
2023-11-15 09:07:13 +0000

    Added limits, grid, multiple lines on same chart and comments

--- Matplotlib-Practice.py
+++ Matplotlib-Practice.py
@@ -7,42 +7,45 @@ plt.plot([23,14,43,53],
          [14,23,29,41],
          color = 'purple',
          linestyle='--',
-         linewidth=2,
-         marker = 'd',
-         markersize=10,
+         linewidth=5,
+         marker = 'o',
+         markersize=12,
          label='Water')
 plt.annotate('Wrong Value',
             xy=(14,23),
             xytext=(20,30), color='red',
             arrowprops=dict(facecolor='r', edgecolor='black', alpha=0.5))
 plt.title('Playing with Plots')
-plt.xlabel('time', fontsize=15, color='green')
-plt.ylabel('temperature', fontsize=15, color="green")
+plt.xlabel('Time', fontsize=15, color='green')
+plt.ylabel('Temperature', fontsize=15, color="green")
 plt.tick_params(axis='y',
-                color='red',
-                labelcolor='green',
-                labelsize='xx-large')
-plt.legend()
+                color='green',
+                labelcolor='red',
+                labelsize='x-large')
+plt.tick_params(axis='x',
+                color='green',
+                labelcolor='red',
+                labelsize='x-large')
 plt.show()
 
 
 
-#Working with CSV file
 
+# Working with CSV file
 data = pd.read_csv("export_1662385061.csv") # Load Data
 
-# Convert 'timestamp' to datetime if it's not already
+# Convert 'timestamp' to datetime
 data['timestamp'] = pd.to_datetime(data['timestamp'])
 
 # Start a new figure
-plt.figure(figsize=(20, 5))
+plt.figure(figsize=(19, 5))
 
 # First y-axis for 'Exciter Field Current'
-line1, = plt.plot(data['timestamp'], data['Exciter Field Current'], color='purple', linestyle='--', linewidth=2, marker='d', markersize=10, label='Exciter Field Current')
+line1, = plt.plot(data['timestamp'], data['Exciter Field Current'], color='purple', linestyle='--', linewidth=1, label='Exciter Field Current')
 
 # Second y-axis for 'Line Current'
 ax2 = plt.twinx()
-line2, = ax2.plot(data['timestamp'], data['Line Current'], color='green', linewidth=2, marker='o', markersize=10, label='Line Current')
+line2, = ax2.plot(data['timestamp'], data['Line Current'], color='green', linewidth=1,  label='Line Current')
 
 # Set the labels for x-axis and y-axes
 plt.xlabel("Time (ISO 8601)")
@@ -55,6 +58,10 @@ ax2.tick_params(axis='y', colors='green')
 
 # Create legends for both lines
 plt.legend([line1, line2], ["Exciter Field Current", "Line Current"], loc='upper left')
+
+# Set limit for the y-axis
+plt.ylim(40)
+ax2.set_ylim()
 
 # Add title
 plt.title('Exciter Field Current and Line Current Over Time')
